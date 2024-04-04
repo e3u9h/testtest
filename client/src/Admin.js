a1ee0a34b5d4edd7d860101622386aa3cd5f8c49
e3u9h <dfz2003@126.com>
2024-04-05 07:46:32 +0800

    update

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -2,57 +2,56 @@ import { Container } from '@material-ui/core';
 import * as React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useState } from 'react';
-import {BACK_END} from './App';
+import { BACK_END } from './App';
 import { Link } from 'react-router-dom';
 
 class Admin extends React.Component {
 
-    render() {
-        return (<>
-            <Container fluid>
-                <div id="scrollableDiv" className='border' style={{ height: "100vh", overflow: "auto" }}>
-                    <ListUser/>
-                    <DeleteUser />
-                </div>
-            </Container>
-        </>
-        );
-    }
+  render() {
+    return (<>
+      <Container fluid>
+        <div id="scrollableDiv" className='border' style={{ height: "100vh", overflow: "auto" }}>
+          <ListUser />
+        </div>
+      </Container>
+    </>
+    );
+  }
 
 }
 
 class ListUser extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = { userList: []};
-    }
-  
-  async getAllUser(){
-      let res = await fetch(BACK_END + 'reportusers',{
-        method:'GET',
-        headers: { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      });
-      let l = await res.json();
-      await this.setState({userList:l});
-      console.log(this.state.userList);
-    }
-    componentDidMount(){
-      this.getAllUser()
-    }
+    super(props);
+    this.state = { userList: [] };
+  }
+
+  async getAllUser() {
+    let res = await fetch(BACK_END + 'reportusers', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    });
+    let l = await res.json();
+    await this.setState({ userList: l });
+    console.log(this.state.userList);
+  }
+  componentDidMount() {
+    this.getAllUser()
+  }
 
   render() {
-      return (<>
-          <InfiniteScroll dataLength={this.state.userList.length} next={null} hasMore={false} scrollableTarget="scrollableDiv"
-              endMessage={<p style={{ textAlign: 'center' }}>
-                  <h6>That's all users :)</h6>
-                  <UserListView userInfos={this.state.userList}/>
-              </p>}>
-          </InfiniteScroll>
-      </>
-      );
+    return (<>
+      <InfiniteScroll dataLength={this.state.userList.length} next={null} hasMore={false} scrollableTarget="scrollableDiv"
+        endMessage={<p style={{ textAlign: 'center' }}>
+          <h6>That's all users :)</h6>
+          <UserListView userInfos={this.state.userList} />
+        </p>}>
+      </InfiniteScroll>
+    </>
+    );
   }
 }
 
@@ -81,27 +80,27 @@ function UserListView({ userInfos }) {
   const [userInfoList, setUserList] = useState(userInfos);
 
   return (
-      <>
-          <div className="container-fluid border" style={{padding: '20px'}}>
-              <div class="row mb-3">
-                  <h3 id="updateTtitle"> List User </h3>
-              </div>
-              <div className='row g-3 border-bottom' style={{padding: '20px'}}>
-                  <div class="col-auto" style={{width: '50%', textAlign: 'center'}}>
-                      <h5> Username </h5>
-                  </div>
-                      <div class="col-auto" style={{width: '25%'}}>
-                  </div>
-                  <div class="col-auto" style={{width: '25%', textAlign: 'center'}}>
-                      <h5> Operation </h5>
-                  </div>
-                  
-              </div>
-              {userInfos.map((userInfo, index) =>
-                  <UserView name={userInfo.username} id={userInfo.id} key={index} />
-              )}
-          </div >
-      </>
+    <>
+      <div className="container-fluid border" style={{ padding: '20px' }}>
+        <div class="row mb-3">
+          <h3 id="updateTtitle"> List User </h3>
+        </div>
+        <div className='row g-3 border-bottom' style={{ padding: '20px' }}>
+          <div class="col-auto" style={{ width: '50%', textAlign: 'center' }}>
+            <h5> Username </h5>
+          </div>
+          <div class="col-auto" style={{ width: '25%' }}>
+          </div>
+          <div class="col-auto" style={{ width: '25%', textAlign: 'center' }}>
+            <h5> Operation </h5>
+          </div>
+
+        </div>
+        {userInfos.map((userInfo, index) =>
+          <UserView name={userInfo.username} id={userInfo.id} key={index} />
+        )}
+      </div >
+    </>
   );
 
 }
--- client/src/Login.js
+++ client/src/Login.js
@@ -40,13 +40,13 @@ const Login = (props) => {
     const genderRadios = document.getElementsByName("radio-gender");
     const genderRadio = document.querySelector('input[name="radio-gender"]:checked');
     const gender = genderRadio ? genderRadio.value : null;
-    console.log(username,newpwd,gender)
+    console.log(username, newpwd, gender)
     const userInfo = {
       newusername: username,
       newpwd: newpwd,
       gender: gender
     };
-    if (username==='') {
+    if (username === '') {
       window.alert("Please enter a username.");
     } else if (!newpwd || !newpwd2) {
       window.alert("Please enter a password");
@@ -55,28 +55,27 @@ const Login = (props) => {
     } else if (newpwd !== newpwd2) {
       window.alert("Password mismatch!");
     } else if (!gender) {
-        window.alert("Please select a gender.");
+      window.alert("Please select a gender.");
     } else {
-    fetch(BACK_END + "createuser", {
+      fetch(BACK_END + "createuser", {
         method: "POST",
         body: JSON.stringify(userInfo),
         headers: {
           'Content-Type': 'application/json'
         },
       })
-      .then(res => {
-        if (res.status === 201) {
-          setLogin(true);
-          setUsername(username);
-          setMode('user');
-          login(username, 'user');
-        }
-        return res.text();
-      })
-      .then(data => { alert(data); })
-      .catch(err => {
-        console.log(err);
-      });
+        .then(res => {
+          if (res.status === 201) {
+            setLogin(true);
+            setMode('user');
+            login(username, 'user');
+          }
+          return res.text();
+        })
+        .then(data => { alert(data); })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 
@@ -95,7 +94,7 @@ const Login = (props) => {
       username: username,
       pwd: pwd
     };
-    
+
     fetch(BACK_END + "login/user", {
       method: "POST",
       body: JSON.stringify(userInfo),
@@ -106,7 +105,6 @@ const Login = (props) => {
       .then(res => {
         if (res.status === 201) {
           setLogin(true);
-          setUsername(username);
           setMode('user');
           login(username, 'user');
         }
@@ -115,7 +113,6 @@ const Login = (props) => {
       .then(data => {
         if (data == 'Login As Admin Successfully!\n') {
           setLogin(true);
-          setUsername(username);
           setMode('admin');
           login(username, 'admin');
         } alert(data)
@@ -124,123 +121,123 @@ const Login = (props) => {
         console.log(err);
       });
   };
-    return (login === false ? 
-      (<MDBContainer  className="p-3 my-5 d-flex flex-column w-50">
+  return (login === false ?
+    (<MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
-        <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
-          <MDBTabsItem>
-            <MDBTabsLink 
-              onClick={() => handleJustifyClick('login')} 
-              active={justifyActive === 'login'} 
-              color={justifyActive === 'login' ? "secondary" : "light"}
-              className={justifyActive === 'login' ? "text-white" : "text-secondary"}
-            >
-              Login
-            </MDBTabsLink>
-          </MDBTabsItem>
-          <MDBTabsItem>
-            <MDBTabsLink 
-              onClick={() => handleJustifyClick('register')} 
-              active={justifyActive === 'register'} 
-              color={justifyActive === 'register' ? "secondary" : "light"}
-              className={justifyActive === 'register' ? "text-white" : "text-secondary"}
-            >
-              Register
-            </MDBTabsLink>
-          </MDBTabsItem>
-        </MDBTabs>
+      <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick('login')}
+            active={justifyActive === 'login'}
+            color={justifyActive === 'login' ? "secondary" : "light"}
+            className={justifyActive === 'login' ? "text-white" : "text-secondary"}
+          >
+            Login
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick('register')}
+            active={justifyActive === 'register'}
+            color={justifyActive === 'register' ? "secondary" : "light"}
+            className={justifyActive === 'register' ? "text-white" : "text-secondary"}
+          >
+            Register
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
 
-        <MDBTabsContent>
-            
-          <MDBTabsPane show={justifyActive === 'login'}>
+      <MDBTabsContent>
 
-            <div className="form-group mb-4">
-                <label htmlFor="newusername">Username</label>
-                <input type="text" className="form-control" id="username" />
-            </div>
-            <div className="form-group mb-4">
-                <label htmlFor="newpwd">Password</label>
-                <input type="password" className="form-control" id="pwd" />
-            </div>
-            <button 
-                className="mb-4 w-100" 
-                style={{ 
-                    backgroundColor: "#6c757d", 
-                    color: "white", 
-                    fontSize: "17px", 
-                    borderRadius: "4px", 
-                    border: "white",
-                    height: "40px" 
-                }} 
-                onClick={handleUserSubmit}
-            >
-                Login
-            </button>
+        <MDBTabsPane show={justifyActive === 'login'}>
+
+          <div className="form-group mb-4">
+            <label htmlFor="newusername">Username</label>
+            <input type="text" className="form-control" id="username" />
+          </div>
+          <div className="form-group mb-4">
+            <label htmlFor="newpwd">Password</label>
+            <input type="password" className="form-control" id="pwd" />
+          </div>
+          <button
+            className="mb-4 w-100"
+            style={{
+              backgroundColor: "#6c757d",
+              color: "white",
+              fontSize: "17px",
+              borderRadius: "4px",
+              border: "white",
+              height: "40px"
+            }}
+            onClick={handleUserSubmit}
+          >
+            Login
+          </button>
         </MDBTabsPane>
         <MDBTabsPane show={justifyActive === 'register'}>
 
-        <div className="form-group mb-4">
+          <div className="form-group mb-4">
             <label htmlFor="newusername">Username</label>
             <input type="text" className="form-control" id="newusername" />
-        </div>
-        <div className="form-group mb-4">
+          </div>
+          <div className="form-group mb-4">
             <label htmlFor="newpwd">Password</label>
             <input type="password" className="form-control" id="newpwd" />
-        </div>
-        <div className="form-group mb-4">
+          </div>
+          <div className="form-group mb-4">
             <label htmlFor="newpwd2">Recheck Password</label>
             <input type="password" className="form-control" id="newpwd2" />
-        </div>
-            <div className="mb-5">
-                <label htmlFor="gender" className="col-form-label"> Gender: </label>
-                <div className="d-flex">
-                    <div className="form-check me-4">
-                        <input className="form-check-input" type="radio" name="radio-gender" id="radio-male" value="Male" />
-                        <label className="form-check-label" htmlFor="radio-male">
-                        Male
-                        </label>
-                    </div>
-                    <div className="form-check me-4">
-                        <input className="form-check-input" type="radio" name="radio-gender" id="radio-female" value="Female" />
-                        <label className="form-check-label" htmlFor="radio-female">
-                        Female
-                        </label>
-                    </div>
-                    <div className="form-check me-4">
-                        <input className="form-check-input" type="radio" name="radio-gender" id="radio-others" value="Others" />
-                        <label className="form-check-label" htmlFor="radio-others">
-                        Others
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" name="radio-gender" id="radio-nottospecify" value="NottoSpecify" />
-                        <label className="form-check-label" htmlFor="radio-nottospecify">
-                        Not to Specify
-                        </label>
-                    </div>
-                </div>
+          </div>
+          <div className="mb-5">
+            <label htmlFor="gender" className="col-form-label"> Gender: </label>
+            <div className="d-flex">
+              <div className="form-check me-4">
+                <input className="form-check-input" type="radio" name="radio-gender" id="radio-male" value="Male" />
+                <label className="form-check-label" htmlFor="radio-male">
+                  Male
+                </label>
+              </div>
+              <div className="form-check me-4">
+                <input className="form-check-input" type="radio" name="radio-gender" id="radio-female" value="Female" />
+                <label className="form-check-label" htmlFor="radio-female">
+                  Female
+                </label>
+              </div>
+              <div className="form-check me-4">
+                <input className="form-check-input" type="radio" name="radio-gender" id="radio-others" value="Others" />
+                <label className="form-check-label" htmlFor="radio-others">
+                  Others
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="radio" name="radio-gender" id="radio-nottospecify" value="NottoSpecify" />
+                <label className="form-check-label" htmlFor="radio-nottospecify">
+                  Not to Specify
+                </label>
+              </div>
             </div>
-            <button 
-                className="mb-4 w-100" 
-                style={{ 
-                    backgroundColor: "#6c757d",
-                    color: "white", 
-                    fontSize: "17px", 
-                    borderRadius: "4px", 
-                    border: "white",
-                    height: "40px"
-                }} 
-                onClick={handleRegister}
-            >
-                Register
-            </button>
+          </div>
+          <button
+            className="mb-4 w-100"
+            style={{
+              backgroundColor: "#6c757d",
+              color: "white",
+              fontSize: "17px",
+              borderRadius: "4px",
+              border: "white",
+              height: "40px"
+            }}
+            onClick={handleRegister}
+          >
+            Register
+          </button>
 
         </MDBTabsPane>
 
-        </MDBTabsContent>
+      </MDBTabsContent>
 
-      </MDBContainer>) :(mode === 'user' ? <Navigate to='/'/> : <Navigate to='/admin'/>)
-    );
+    </MDBContainer>) : (mode === 'user' ? <Navigate to='/' /> : <Navigate to='/admin' />)
+  );
 }
 export default Login;
 
