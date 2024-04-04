26e5d8605ed6dbea271b0e9b68f5d3e2b838ce3c
06zoeyyy <1155157043@link.cuhk.edu.hk>
2024-04-05 00:12:43 +0800

    listalluser

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useState } from 'react';
 import {BACK_END} from './App';
+import { Link } from 'react-router-dom';
 
 class Admin extends React.Component {
 
@@ -11,6 +12,7 @@ class Admin extends React.Component {
             <Container fluid>
                 <div id="scrollableDiv" className='border' style={{ height: "100vh", overflow: "auto" }}>
                     <ListUser/>
+                    <DeleteUser />
                 </div>
             </Container>
         </>
@@ -46,6 +48,7 @@ class ListUser extends React.Component {
           <InfiniteScroll dataLength={this.state.userList.length} next={null} hasMore={false} scrollableTarget="scrollableDiv"
               endMessage={<p style={{ textAlign: 'center' }}>
                   <h6>That's all users :)</h6>
+                  <UserListView userInfos={this.state.userList}/>
               </p>}>
           </InfiniteScroll>
       </>
@@ -53,6 +56,54 @@ class ListUser extends React.Component {
   }
 }
 
+const UserView = (props) => {
+  return (
+    <form className="row g-3 border-bottom" style={{ padding: '20px' }}>
+      <div className="col-auto" style={{ width: '50%', textAlign: 'center' }}>
+        <label htmlFor="interest" className="col-form-label" style={{ fontWeight: 'bold' }}>
+          {props.name}
+        </label>
+      </div>
+      <div className="col-auto" style={{ width: '25%' }}></div>
+      <div className="col-auto" style={{ width: '25%', textAlign: 'center' }}>
+        <Link to={`/${props.name}`}>
+          <button type="button" className="btn btn-secondary">
+            View Details
+          </button>
+        </Link>
+      </div>
+    </form>
+  );
+};
 
+function UserListView({ userInfos }) {
+
+  const [userInfoList, setUserList] = useState(userInfos);
+
+  return (
+      <>
+          <div className="container-fluid border" style={{padding: '20px'}}>
+              <div class="row mb-3">
+                  <h3 id="updateTtitle"> List User </h3>
+              </div>
+              <div className='row g-3 border-bottom' style={{padding: '20px'}}>
+                  <div class="col-auto" style={{width: '50%', textAlign: 'center'}}>
+                      <h5> Username </h5>
+                  </div>
+                      <div class="col-auto" style={{width: '25%'}}>
+                  </div>
+                  <div class="col-auto" style={{width: '25%', textAlign: 'center'}}>
+                      <h5> Operation </h5>
+                  </div>
+                  
+              </div>
+              {userInfos.map((userInfo, index) =>
+                  <UserView name={userInfo.username} id={userInfo.id} key={index} />
+              )}
+          </div >
+      </>
+  );
+
+}
 
 export { Admin };
