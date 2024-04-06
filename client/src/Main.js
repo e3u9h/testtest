67fdcf17adb50a03fe3ee26b0eeb2e152e6b5b8a
zhouyaaaaa <zhou_jieya@163.com>
2024-04-06 18:15:44 +0800

    update main

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,8 +1,42 @@
-
-
+import {useState, useRef, useEffect} from 'react'
 
 
 function NewPost() {
+  const[availableTags, setAvailableTags] = useState([]);
+  const[tags, setTags] = useState([]);
+  const [privacy, setPrivacy] = useState('false');
+  const [fileList, setFileList] = useState([]);
+  const [postContent, setPostContent] = useState('');
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+
+  const fetchAvaliableTags = () => {
+    fetch('http://localhost:8000tags',{
+      mothod: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).then(res => res.json()).then(data => {
+      const fetchedTags = data.map((item) => item['tag']);
+      setAvailableTags(fetchedTags);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  
+
+
+
+  
+
+
+
+
+
+
+
+
  
 
   return (
--- client/src/Tweet.js
+++ client/src/Tweet.js
@@ -268,7 +268,7 @@ function ForwardForm(props) {
   const [privacy, setPrivacy] = useState('false');
   const [repostContent, setRepostContent] = useState('')
 
-  
+
   const fetchAvailableTags = () => {
     fetch('http://localhost:8000/tags', {
       method: 'GET',
@@ -430,4 +430,4 @@ function TweetListView({ tweetInfos }) {
     </>)
 }
 
-export { TweetListView, TweetCard };
+export { TweetListView, TweetCard, ForwardForm};
