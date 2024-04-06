c5af307a8160510f4ecb5447754e9271368ba9f7
06zoeyyy <1155157043@link.cuhk.edu.hk>
2024-04-06 23:00:15 +0800

    adminadduser

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -11,7 +11,9 @@ class Admin extends React.Component {
     return (<>
       <Container fluid>
         <div id="scrollableDiv" className='border' style={{ height: "100vh", overflow: "auto" }}>
+          <AddUser/>
           <ListUser />
+          <DeleteUser />
         </div>
       </Container>
     </>
@@ -20,13 +22,66 @@ class Admin extends React.Component {
 
 }
 
+class AddUser extends React.Component {
+
+  handleSubmit = (event) => {
+      const username = document.getElementById("addUsername").value;
+      const newpwd = document.getElementById("addPassword").value;
+      const userInfo = {
+        newusername: username,
+        newpwd: newpwd
+      };
+      fetch(BACK_END+"createuser", {
+        method: "POST",
+        body: JSON.stringify(userInfo),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+        .then(res => {
+          if (res.status === 201) {
+            alert("Successfully create user");
+          }
+          return res.text();
+        }).then(data => {
+          alert(data);
+          window.location.reload(true);
+      }).catch(err => {
+          console.log(err);
+        });
+      event.preventDefault();
+  
+    } 
+  renderFormGroup = (label, id, type) => (
+    <div className="row mb-3">
+      <label htmlFor={id} className="col-sm-2 col-form-label">{label}</label>
+      <div className="col-sm-10">
+        <input type={type} className="form-control" id={id} />
+      </div>
+    </div>
+  );
+  
+  render() {
+    return (
+      <div className='border' style={{padding: '20px'}}>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row mb-3">
+            <h3 id="addTtitle">Add User</h3>
+          </div>
+          {this.renderFormGroup("Username:", "addUsername", "text")}
+          {this.renderFormGroup("Password:", "addPassword", "password")}
+          <button type="submit" className="btn btn-success">Create</button>
+        </form>
+      </div>
+    );
+  }
+}  
 class ListUser extends React.Component {
   constructor(props) {
     super(props);
     this.state = { userList: [] };
   }
 
-<<<<<<< HEAD
   componentDidMount() {
     this.getAllUser();
   }
@@ -61,33 +116,6 @@ class ListUser extends React.Component {
           </p>
         }>
       </InfiniteScroll>
-=======
-  async getAllUser() {
-    let res = await fetch(BACK_END + 'reportusers', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    });
-    let l = await res.json();
-    await this.setState({ userList: l });
-    console.log(this.state.userList);
-  }
-  componentDidMount() {
-    this.getAllUser()
-  }
-
-  render() {
-    return (<>
-      <InfiniteScroll dataLength={this.state.userList.length} next={null} hasMore={false} scrollableTarget="scrollableDiv"
-        endMessage={<p style={{ textAlign: 'center' }}>
-          <h6>That's all users :)</h6>
-          <UserListView userInfos={this.state.userList} />
-        </p>}>
-      </InfiniteScroll>
-    </>
->>>>>>> a1ee0a34b5d4edd7d860101622386aa3cd5f8c49
     );
   }
 }
