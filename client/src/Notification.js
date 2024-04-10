2ee6834a4f1c79cff67b20bd13f517272e276d61
06zoeyyy <1155157043@link.cuhk.edu.hk>
2024-04-11 01:00:00 +0800

    noti

--- client/src/Notification.js
+++ client/src/Notification.js
@@ -1,5 +1,19 @@
 import * as React from 'react';
 
+const iconMap = {
+    "like": faThumbsUp,
+    "comment": faComment,
+    "retweet": faRetweet,
+    "follow": faUser
+}
+
+const actionMap = {
+    "like": "likes your tweet",
+    'follow': "starts to follow you",
+    "comment": "comments on your tweet",
+    "retweet": "retweets your tweet" 
+}
+
 class Notification extends React.Component{
     constructor(props){
         super(props)
