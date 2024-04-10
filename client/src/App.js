d168815043d189c12929a2b435dd7dc398cef4e0
Tonkmy11 <1155174447@link.cuhk.edu.hk>
2024-04-11 04:58:46 +0800

    import Messenger to App

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ import SearchTweet from './SearchTweet';
 import SearchUserid from './Searchid';
 import SearchTweetByKeyword from './SearchTweetByKeyword';
 import cookie from 'react-cookies';
-import Messenger from './messenger/Messenger';
+import Messenger from './Messenger';
 
 
 export const BACK_END = 'http://localhost:8000/'
