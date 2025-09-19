fb690b505e5d2914ab754d95b9d5dd7247382862
e3u9h <dfz2003@126.com>
2025-09-19 03:26:55 -0500

    update

--- start-with-redis.bat
+++ start-with-redis.bat
@@ -1,16 +1,7 @@
 @echo off
 echo Starting Redis integration setup...
 
-REM 检查 .env 文件是否存在
-if not exist ".env" (
-    echo  .env file not found. Creating default .env file...
-    echo MONGO_URI=mongodb+srv://qwerty:qwer4321@cluster0.5gm5w78.mongodb.net/?retryWrites=true^&w=majority^&appName=Cluster0 > .env
-    echo JWT_KEY="jwtkey111111" >> .env
-    echo REDIS_URL=redis://localhost:6379 >> .env
-    echo Default .env file created
-) else (
-    echo  .env file found
-)
+
 
 REM 检查 Docker 是否运行
 docker info >nul 2>&1
